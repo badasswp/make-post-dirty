ad204cdbbc3975177310e4dc5c8d28c4f31dac6d
badasswp <badasswpdev@gmail.com>
2025-10-20 10:25:01 +0000

    push to master-built

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/make-post-dirty',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ce6233443b6cdc94a57ac38003fe63e93790b190',
+        'type' => 'package',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/make-post-dirty' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ce6233443b6cdc94a57ac38003fe63e93790b190',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
