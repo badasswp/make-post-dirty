5040aed6003fe21d0798ab1c71e97460da0a0c9e
badasswp <badasswpdev@gmail.com>
2026-05-06 20:51:50 +0000

    Build from release-1.2.0

--- src/utils/interfaces.tsx
+++
@@ -1,4 +0,0 @@
-export interface Post {
-	attribute: string;
-	value: string;
-}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/make-post-dirty',
+        'pretty_version' => 'dev-release-1.2.0-built',
+        'version' => 'dev-release-1.2.0-built',
+        'reference' => '3910529f06623a863676ca09e969e9483bd82eb3',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/make-post-dirty' => array(
+            'pretty_version' => 'dev-release-1.2.0-built',
+            'version' => 'dev-release-1.2.0-built',
+            'reference' => '3910529f06623a863676ca09e969e9483bd82eb3',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
