e3905f345e2fcba76b9428e318a981ac45ca35b4
badasswp <badasswpdev@gmail.com>
2026-05-22 20:20:55 +0000

    Build from release-1.2.2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/make-post-dirty',
+        'pretty_version' => 'dev-release-1.2.2-built',
+        'version' => 'dev-release-1.2.2-built',
+        'reference' => 'c2f614922ecc14f6b928c30a6b2908008beb8c5f',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/make-post-dirty' => array(
+            'pretty_version' => 'dev-release-1.2.2-built',
+            'version' => 'dev-release-1.2.2-built',
+            'reference' => 'c2f614922ecc14f6b928c30a6b2908008beb8c5f',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
