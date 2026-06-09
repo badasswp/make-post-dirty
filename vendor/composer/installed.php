aca183c44cbc7dd6a61ce28165d3b76ebebedd47
badasswp <badasswpdev@gmail.com>
2026-06-09 11:24:48 +0000

    Build from release-1.3.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/make-post-dirty',
+        'pretty_version' => 'dev-release-1.3.0-built',
+        'version' => 'dev-release-1.3.0-built',
+        'reference' => '651b00e9d4cd11114e9ef9ae87d7f9ef6f5f4977',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/make-post-dirty' => array(
+            'pretty_version' => 'dev-release-1.3.0-built',
+            'version' => 'dev-release-1.3.0-built',
+            'reference' => '651b00e9d4cd11114e9ef9ae87d7f9ef6f5f4977',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'badasswp/pluginate' => array(
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => '2b28213dcc47ce0dc524e904a4bc8f51341fd5b5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../badasswp/pluginate',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
