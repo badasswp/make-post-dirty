0e78676b09203434262512d2d02fad3293e2d4db
badasswp <badasswpdev@gmail.com>
2026-06-22 09:19:16 +0000

    push to master-built

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/make-post-dirty',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'fcbef841e27020a4b2b819e09e3ef29b5e71e271',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/make-post-dirty' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'fcbef841e27020a4b2b819e09e3ef29b5e71e271',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'badasswp/pluginate' => array(
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => '2b28213dcc47ce0dc524e904a4bc8f51341fd5b5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../badasswp/pluginate',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
